1fabd0639d7f7df24df9d3ac909335a809428069
Ikjot <ikjotkhurana@gmail.com>
2020-05-23 00:00:16 +0530

    Notify App Testing unit int mismatch error

--- app/src/main/java/com/example/notify/AddNotesActivity.kt
+++ app/src/main/java/com/example/notify/AddNotesActivity.kt
@@ -2,11 +2,55 @@ package com.example.notify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class AddNotesActivity : AppCompatActivity() {
+
+    private lateinit var  titleED:EditText
+    private lateinit var descriptionED:EditText
+    private lateinit var saveNotesBtn:Button
+    private lateinit var realm: Realm
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
+    //init views
+
+        realm = Realm.getDefaultInstance()
+        titleED = findViewById(R.id.title_EditText)
+        descriptionED = findViewById(R.id.description_EditText)
+        saveNotesBtn = findViewById(R.id.saveNotesbtn)
+
+        // onclick listner
+
+        saveNotesBtn.setOnClickListener {
+
+            addNotesTODB()
+        }
+
+
+
+    }
+
+    private fun addNotesTODB() {
+        // auto increment ID
+        try {
+            realm.beginTransaction()
+            val currentIdNumber: Number? = realm.where(Notes::class.java).max("id")
+            val nextID:Int
+            nextID = if(currentIdNumber==null){
+
+            }else{
+                currentIdNumber.toInt() + 1
+            }
+        }catch (e:Exception){
+
+        }
+
     }
 }
